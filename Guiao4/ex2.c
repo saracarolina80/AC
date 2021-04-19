4367639bbe018b441ae272ed71df237afddfc52e
saracarolina80 <sccg@ua.pt>
2021-04-19 10:04:41 +0100

    Guiao 4

---
+++ Guiao4/ex2.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+
+static unsigned int numOps;
+
+int check ( int* array , int n) {
+    int i;
+    int j;
+    int k;
+    int result = 0;
+    for ( k = 2 ; k < n  ; k++) {
+        for (j = 1; j < k  ; j++) {
+            for (i = 0; i < j ; i++) {
+                if (array[k] == array[i] + array[j]) {
+                    result++;
+                }
+                numOps++;
+            }
+        }
+    }
+    return result;
+}
+
+void printArray(int* array, int n) {
+    printf("[");
+
+    int i;
+
+    for( i = 0 ; i < n  ; i++) {
+        printf(" %d,",array[i] );
+    }
+    printf("]\n");
+}
+
+int main(void) {
+
+    int res;
+
+    // TESTE
+    int a1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int a2[10] = {1, 2, 1, 4, 5, 6, 7, 8, 9, 10};
+    int a3[10] = {1, 2, 1, 3, 2, 6, 7, 8, 9, 10};
+    int a4[10] = {0, 2, 2, 0, 3, 3, 0, 4, 4, 0};
+    int a5[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int a6[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+
+    int size = 10;
+    int size2 = 20;
+
+    printf("----TESTING-----\n");
+
+    // a1
+    numOps = 0;
+
+    res = check(a1 , size);
+
+    printf("For array 1: \n");
+
+    printArray(a1 , size);
+
+    printf("Result = %d \t Num_Operations = %d \n" , res , numOps);
+
+    // a2
+
+    printf("For array 2: \n");
+
+    printArray(a2 , size);
+
+    numOps = 0;
+
+    res = check(a2 , size);
+
+    printf("Result = %d \t Num_Operations = %d \n" , res , numOps);
+
+    // a3
+    printf("For array 3: \n");
+
+    printArray(a3 , size);
+
+    numOps = 0;
+
+    res = check(a3 , size);
+
+    printf("Result = %d \t Num_Operations = %d \n" , res , numOps);
+
+    // a4
+
+    printf("For array 4: \n");
+
+    printArray(a4 , size);
+
+    numOps = 0;
+
+    res = check(a4 , size);
+
+    printf("Result = %d \t Num_Operations = %d \n" , res , numOps);
+
+    //a5
+    printf("For array 5: \n");
+
+    printArray(a5 , size);
+
+    numOps = 0;
+
+    res = check(a5, size);
+
+    printf("Result = %d \t Num_Operations = %d \n" , res , numOps);
+
+    //a6
+    printf("For array 6: \n");
+
+    printArray(a6 , size2);
+
+    numOps = 0;
+
+    res = check(a6, size2);
+
+    printf("Result = %d \t Num_Operations = %d \n" , res , numOps);
+
+}
